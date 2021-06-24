da40ed0061531a9737cfda38e63df3649886e0e9
NQLong <62022783+NQLong@users.noreply.github.com>
2021-06-24 14:12:59 +0700

    init

---
+++ conflict.py
@@ -0,0 +1,11 @@
+def find(lst, sum):
+    #find indexes of 2 elements that sum up to $sum
+    pass
+
+def main():
+    lst = [1, 2, 3, 4, 5, 6, 7]
+    sum = 10
+    print(find(lst, sum))
+
+if __name__ == "__main__":
+    main()
