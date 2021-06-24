feaab5d5205a86aa883f3c77fd55f81edce60f73
nqlong <nqlong0709@gmail.com>
2021-06-24 14:16:37 +0700

    B solutions

--- conflict.py
+++ conflict.py
@@ -1,11 +1,17 @@
 def find(lst, sum):
-    #find indexes of 2 elements that sum up to $sum
-    pass
-
+    # find indexes of 2 elements that sum up to $sum
+    my_hash_table = dict()
+    for i, val in enumerate(lst):
+        my_hash_table[f"{val}"] = i
+    for i, val in enumerate(lst):
+        other = my_hash_table.get(f"{sum - val}")
+        if other:return (i,other)
+    
 def main():
     lst = [1, 2, 3, 4, 5, 6, 7]
     sum = 10
     print(find(lst, sum))
+
 
 if __name__ == "__main__":
     main()
