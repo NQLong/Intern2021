a6877d42bc600df2ed8094f20a6c535b097d03e1
nqlong <nqlong0709@gmail.com>
2021-06-24 14:15:15 +0700

    A solution

--- conflict.py
+++ conflict.py
@@ -1,6 +1,11 @@
 def find(lst, sum):
     #find indexes of 2 elements that sum up to $sum
-    pass
+    try:
+        for i, val in enumerate(lst):
+            for k, other in enumerate(lst[i+1:]):
+                if val + other == sum:return (i,k +i+1)
+    except Exception as err:
+        return None
 
 def main():
     lst = [1, 2, 3, 4, 5, 6, 7]
